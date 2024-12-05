b0ca4ea21d110582bb459a23388b209e2a1dd206
ToanNguyenIFI <Toan11.NguyenThi@nttdata.com>
2024-12-05 17:51:46 +0700

    initial commit

---
+++ src/test/runner/TestRunner.java
@@ -0,0 +1,20 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+  features = "src/test/java/features",
+  glue = {"steps"},
+//        tags = "localRunOnly",
+  plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber-report/cucumber.json", "junit:target/cucumber-report/cucumber.xml", "timeline:target/timeline", "rerun:target/failureScenarios.txt", "steps.StepDetails"}
+)
+
+public class TestRunner extends AbstractTestNGCucumberTests {
+  @Override
+  @DataProvider(parallel = true)
+  public Object[][] scenarios() {
+    return super.scenarios();
+  }
+}
